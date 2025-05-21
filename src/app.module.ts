81caa2ab8084efd9970e5b1bfca2cb6e68257181
Jmjoss <josekandia42@gmail.com>
2025-05-21 00:40:17 -0300

    chore: add database postgre with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,31 @@ import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule, UsersModule, AuthModule, NotificationsModule],
+  imports: [
+    TasksModule,
+    UsersModule,
+    AuthModule,
+    NotificationsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRES_HOST', 'localhost'),
+        port: parseInt(config.get<string>('POSTGRES_PORT', '5433')),
+        username: config.get<string>('POSTGRES_USER', 'user_crud'),
+        password: config.get<string>('POSTGRES_PASSWORD', 'root'),
+        database: config.get<string>('POSTGRES_DB', 'db_crud'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
