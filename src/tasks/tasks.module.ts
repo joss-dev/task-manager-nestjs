caae96880f14ca8535235707b95b00be14944e3b
Jmjoss <josekandia42@gmail.com>
2025-05-21 11:34:24 -0300

    chore: add commits

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,10 +4,18 @@ import { TasksController } from './tasks.controller';
 import { TaskRepository } from './repository/tasks.repository';
 import { Task } from './entities/task.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ITaskRepository } from '../interfaces/task-repository.interface';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService, TaskRepository],
+  providers: [
+    TasksService,
+    TaskRepository,
+    {
+      provide: ITaskRepository,
+      useClass: TaskRepository,
+    },
+  ],
 })
 export class TasksModule {}
